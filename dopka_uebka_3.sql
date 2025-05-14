c4f7b70ba8b2ce1383c2e9e461747b0c29fa527b
FastBery <Falls.ignatev@mail.ru>
2025-05-14 09:57:49 +0300

    adsfdgshdtjftk

--- dopka_uebka_3.sql
+++ dopka_uebka_3.sql
@@ -70,9 +70,47 @@ GROUP BY student_id) A
 WHERE student_id = 2
 
 --iiiiiiiiiiiiiiiiiiiii
-
+SELECT 
+    s.name AS student_first_name,
+    s.last_name AS student_last_name,
+    (SELECT AVG(mark) FROM marks WHERE student_id = s.id) AS average_mark
+FROM 
+    students s
+ORDER BY 
+    s.name DESC,          -- по убыванию по первому полю (имя)
+    s.last_name ASC,      -- по возрастанию по второму полю (фамилия)
+    average_mark DESC;    -- по убыванию по третьему полю (средний балл)
 
 --jjjjjjjjjjjjjjjjjjjjj
 SELECT last_name
 FROM students
 WHERE last_name LIKE '_a%'
+
+--kkkkkkkkkkkkkkkkkkkkk
+SELECT 
+    s.id,
+    s.name + ' ' + s.last_name AS full_name,
+    d.name AS department,
+    (SELECT AVG(mark) FROM marks WHERE student_id = s.id) AS average_mark,
+    '2023/2024 Academic Year' AS academic_year  -- Константное поле для всех записей
+FROM 
+    students s
+JOIN 
+    department d ON s.department_id = d.id
+WHERE 
+    (SELECT COUNT(*) FROM marks WHERE student_id = s.id) >= 3  -- Условие отбора
+    AND (SELECT AVG(mark) FROM marks WHERE student_id = s.id) > 75
+ORDER BY 
+    average_mark DESC;  -- Сортировка по убыванию среднего балла
+
+--lllllllllllllllllllll
+SELECT 
+    name + ' ' + last_name as full_name,
+    (SELECT MAX(mark) FROM marks WHERE students.id = student_id) AS max_mark
+FROM students
+ 
+--mmmmmmmmmmmmmmmmmmmmm
+SELECT * 
+FROM marks m 
+INNER JOIN students s ON s.id = m.student_id
+WHERE mark > 85 AND mark < 90 OR subjects_id = 3
